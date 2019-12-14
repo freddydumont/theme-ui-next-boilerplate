6d17d16101253e90780c12b7a805369f1f47f6e7
Frederick Morin <frederickmorin18@gmail.com>
2019-12-14 11:02:36 -0500

    Replace theme with theme-ui/presets

--- src/theme.js
+++ src/theme.js
@@ -1,40 +1,14 @@
+import { swiss, deep } from '@theme-ui/presets';
+
 export default {
+  ...swiss,
   useCustomProperties: true,
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#07c',
-    secondary: '#609',
+    ...swiss.colors,
     modes: {
       dark: {
-        text: '#fff',
-        background: '#222',
-        primary: '#0cf',
-        secondary: '#90c',
+        ...deep.colors,
       },
     },
   },
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: 'inherit',
-  },
-  styles: {
-    root: {
-      fontFamily: 'body',
-      color: 'text',
-      bg: 'background',
-    },
-    h1: {
-      fontSize: [4, 5, 6],
-      color: 'primary',
-    },
-    a: {
-      color: 'primary',
-      textDecoration: 'none',
-      ':hover': {
-        color: 'secondary',
-        textDecoration: 'underline',
-      },
-    },
-  },
-}
+};
