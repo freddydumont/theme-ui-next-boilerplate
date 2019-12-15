900832867ee6f30567f78fb92c390fa4b95ea83d
Frederick Morin <frederickmorin18@gmail.com>
2019-12-14 20:02:08 -0500

    Add theme-ui components

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import { useColorMode } from 'theme-ui'
+import React from 'react';
+import Link from 'next/link';
+import { useColorMode } from 'theme-ui';
+import { Container, Flex, NavLink, Button } from '@theme-ui/components';
 
 export default function Header() {
-  const [colorMode, setColorMode] = useColorMode()
+  const [colorMode, setColorMode] = useColorMode();
   return (
-    <header>
-      <button
-        onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-      </button>
-    </header>
-  )
+    // see theme.layout.container for styles
+    <Container as="header">
+      <Flex as="nav">
+        {/* passHref is required with NavLink */}
+        <Link href="/" passHref>
+          <NavLink p={2}>Home</NavLink>
+        </Link>
+        <Link href="/hello" passHref>
+          <NavLink p={2}>Hello</NavLink>
+        </Link>
+        <Button
+          ml="auto"
+          onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')}
+        >
+          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
--- src/theme.js
+++ src/theme.js
@@ -3,12 +3,26 @@ import { swiss, deep } from '@theme-ui/presets';
 export default {
   ...swiss,
   useCustomProperties: true,
+  useColorSchemeMediaQuery: true,
   colors: {
     ...swiss.colors,
     modes: {
       dark: {
         ...deep.colors,
       },
+    },
+  },
+  layout: {
+    container: {
+      maxWidth: 1024,
+      mx: 'auto',
+      py: 3,
+      px: 4,
+    },
+  },
+  buttons: {
+    primary: {
+      cursor: 'pointer',
     },
   },
 };
