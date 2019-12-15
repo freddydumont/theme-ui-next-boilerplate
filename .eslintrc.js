5b59602b9aece4a93b9ebeb4904fa49c675ac2cf
Frederick Morin <frederickmorin18@gmail.com>
2019-12-15 10:07:53 -0500

    Add typescript and fix dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  parser: 'babel-eslint',
-  extends: [`react-app`],
+  parser: '@typescript-eslint/parser',
+  extends: [`react-app`, 'plugin:@typescript-eslint/recommended'],
   plugins: ['react-hooks'],
   rules: {
     // Next.js doesn't require react to be in scope
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
