30ef99f05cc16ebc59d1b7b581d7aa39b0d82464
Frederick Morin <frederickmorin18@gmail.com>
2020-05-15 13:32:54 -0400

    Fix CI and page test folder location

--- src/__tests__/stylePage.test.tsx
+++ src/__tests__/stylePage.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'test-utils';
-import StylePage from '../style';
+import StylePage from '../pages/style';
 
-describe('Header', () => {
+describe('Style Page', () => {
   it('should render without crashing', () => {
     const { container } = render(<StylePage />);
     expect(container).toBeInTheDocument();
